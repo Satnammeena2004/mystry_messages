faae40670a7d91a102c32d6bd9cafc6ce8cb8101
Satnammeena2004 <satnam.meena2004@gmail.com>
2024-11-19 09:40:30 +0530

    create user model

---
+++ src/models/User.ts
@@ -0,0 +1,60 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface MessageType extends Document {
+  content: string;
+  createAt: Date;
+}
+
+export interface UserType extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVarified: boolean;
+  isAcceptsMessage: boolean;
+  messages: MessageType[];
+}
+
+const MessageSchema: Schema<MessageType> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UserSchema: Schema<UserType> = new Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "varify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "varifyExpiry code is required"],
+  },
+  isVarified: { type: Boolean, default: false },
+  isAcceptsMessage: { type: Boolean, default: true },
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  mongoose.models.User || mongoose.model<UserType>("User", UserSchema);
+
+export default UserModel;
