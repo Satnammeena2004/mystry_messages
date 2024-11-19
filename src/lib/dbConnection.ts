9db41e06e17260d582d7eba53d9e1603606dea58
Satnammeena2004 <satnam.meena2004@gmail.com>
2024-11-19 14:43:49 +0530

    db connection

---
+++ src/lib/dbConnection.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect() {
+  if (connection.isConnected) {
+    console.log("db is already connected");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {
+      dbName: "mystry_messages",
+    });
+    connection.isConnected = db.connections[0].readyState;
+    console.log("db connected successfully");
+  } catch (error) {
+    console.log("db connection failed", error);
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
